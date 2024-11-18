055794b140e1da58939a171a739faa0780580bb3
NIHALA THESNI.K <nihalathesni14@gmail.com>
2024-11-18 10:15:24 +0530

    array

---
+++ array/array.js
@@ -0,0 +1,30 @@
+var fidha=["Fathima Fidha",18,"female"]
+var nihala=["Nihala Thesni",21,"female"]
+var anfar=["Muhammed Anfar",24,"male"]
+var shaheen=["Muhammed Shaheen",19,"male"]
+var salaah=["Muhammed Salaah",23,"male"]
+var abhishek=["Abhishek",20,"male"]
+
+for(i=0;i<fidha.length;i++){
+    console.log(fidha[i])
+}
+for(i=0;i<nihala.length;i++){
+    console.log(nihala[i])
+}
+for(i=0;i<anfar.length;i++){
+    console.log(anfar[i])
+}
+for(i=0;i<shaheen.length;i++){
+    console.log(shaheen[i])
+}
+for(i=0;i<salaah.length;i++){
+    console.log(salaah[i])
+}
+for(i=0;i<abhishek.length;i++){
+    console.log(abhishek[i])
+}
+
+
+
+
+
