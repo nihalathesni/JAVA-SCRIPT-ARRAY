2762d5133c3540e4cfbab9eb2fa9a3f7bffa774b
NIHALA THESNI.K <nihalathesni14@gmail.com>
2024-12-02 11:32:45 +0530

    array

---
+++ sum/sum.js
@@ -0,0 +1,17 @@
+//to create another array of multiple of 4
+
+var num =[2,4,1,5,6]
+var b=[]
+for(i=0;i<num.length;i++){
+    var a=num[i]*4
+    b[i]=a
+  
+} 
+  
+//sum of the new array
+ console.log(b);
+ 
+console.log(b[0]+b[1]+b[2]+b[3]+b[4]);
+
+
+ 
